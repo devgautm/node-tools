da397e32040c6b5313e520a28c613c0ad834d272
Dinesh Gautam <dineshgautam01@hotmail.com>
2026-01-07 14:55:26 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export { hashString, hashFile } from "./tools/hash.js";
+export { uuidV4, uuidMany } from "./tools/uuid.js";
+export { parseJson, formatJson, minifyJson, validateJson } from "./tools/json.js";
+export { base64Encode, base64Decode } from "./tools/base64.js";
+export { slugify } from "./tools/slug.js";
+export { now, parseTime } from "./tools/time.js";
+export { systemInfo } from "./tools/system.js";
